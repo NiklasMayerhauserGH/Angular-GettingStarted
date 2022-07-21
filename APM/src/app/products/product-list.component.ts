540a21bb8484f92bc80b0d6754a1cc60b5a51e72
NiklasMayerhauserGH <niklas.mayerhauser@grenzebach.com>
2022-07-21 16:06:48 +0200

    .

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,44 +1,75 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component
 ({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent
+export class ProductListComponent implements OnInit, OnDestroy
 {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = 
-    [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+    errorMessage: string = '';
+    sub!: Subscription;
+
+    private _listFilter: string = '';
+
+    constructor(private productService: ProductService)
+    {
+        
+    }
+
+    get listFilter(): string 
+    {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string)
+    {
+        this._listFilter = value;
+        console.log('In setter:', value);
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
+
+    performFilter(filterBy: string): IProduct[]
+    {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
+    }
 
     toggleImage(): void 
     {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void 
+    {
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy()
+    {
+        this.sub.unsubscribe();
+    }
+
+    onRatingClicked(message: string): void
+    {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
